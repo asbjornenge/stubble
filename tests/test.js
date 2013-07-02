a60de29758e59d6055c517b8c4f29571e2fae015
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-07-02 12:16:50 +0200

    Testing attribute replacement, removed stubble events

--- tests/test.js
+++ tests/test.js
@@ -11,11 +11,12 @@ define([
 ], function (bdd, expect, stubble) {
   with(bdd) {
 
-    function createStub(name) {
+    function createStub(name, textContent) {
         var template = document.createElement("div");
         template.id = name;
+        template.setAttribute('class','{{classes}}')
         template.setAttribute('data-stub',name);
-        var content = document.createTextNode("Hi there {{username}}, and greetings!");
+        var content = document.createTextNode(textContent);
         template.appendChild(content);
         document.body.appendChild(template);
         return template;
@@ -51,7 +52,7 @@ define([
         })
 
         it('Should inject data from passed object', function() {
-            createStub('user');
+            createStub('user', "Hi there {{username}}, and greetings!");
             stubble.load();
             var template = stubble('user', { username : 'asbjornenge' });
             var index    = template.firstChild.nodeValue.indexOf('asbjornenge');
@@ -66,14 +67,87 @@ define([
             stubble.load();
             expect(stubble.templates).to.have.property('users');
             expect(stubble.templates).to.have.property('user');
+            stubble.templates = {}
         })
+    })
+
+    describe('Extracting & replace data', function() {
+        it('Should allow extracting properties via . selector', function() {
+            createStub('user', "Hi there {{user.name}}, and greetings!");
+            stubble.load();
+            var template = stubble('user', { user : { name : 'asbjornenge' } });
+            var index    = template.firstChild.nodeValue.indexOf('asbjornenge');
+            expect(index).to.be.above(-1);
+            stubble.templates = {}
+        })
+
+        it('Should allow extracting properties via [] selector', function() {
+            createStub('user', "Hi there {{user[name]}}, and greetings!");
+            stubble.load();
+            var template = stubble('user', { user : { name : 'asbjornenge' } });
+            var index    = template.firstChild.nodeValue.indexOf('asbjornenge');
+            expect(index).to.be.above(-1);
+            stubble.templates = {}
+        })
+
+        describe('Nested properties', function() {
+            var nestedData = {
+                user : {
+                    name : {
+                        first : 'asbjorn',
+                        last  :  'enge'
+                    },
+                    nicks : ['asbjornenge','meh']
+                }
+            }
+            var testNested = function(content, expected, data) {
+                createStub('user', content);
+                stubble.load();
+                var template = stubble('user', data);
+                var index    = template.firstChild.nodeValue.indexOf(expected);
+                expect(index).to.be.above(-1);
+                stubble.templates = {}
+                return template;
+            }
+
+            it('Should allow extracting nested properties using . selector', function() {
+                testNested("Hi there {{user.name.first}}, and greetings!", "asbjorn", nestedData);
+            })
+            it('Should allow extracting nested properties using [] selector', function() {
+                testNested("Hi there {{user[name][first]}}, and greetings!", "asbjorn", nestedData);
+            })
+            it('Should allow extracting values from arrays', function() {
+                testNested("Hi there {{user[nicks][0]}}, and greetings!", "asbjornenge", nestedData);
+                testNested("Hi there {{user.nicks[1]}}, and greetings!", "meh", nestedData);
+            })
+        })
+
+        describe('Attributes', function() {
+            it('Should also replace attributes', function() {
+                createStub('user');
+                stubble.load();
+                var template = stubble('user', { classes : 'class1' })
+                var classes  = template.getAttribute('class');
+                expect(classes.indexOf('class1')).to.be.above(-1);
+                stubble.templates = {}
+            })
+        })
+    })
+
+    describe('Filters', function() {
+        console.log(stubble.filters)
+
+        // verify filter data object (and it's properties)
+
+        // default filters
+        // custom filter
+        // replacing a filter
+
     })
 
 
     // TEST
-    //    different property injections obj.prop, obj[prop]
     //    @filters
-    //    sub-templates are loaded & handled correctly
 
   }
 })
